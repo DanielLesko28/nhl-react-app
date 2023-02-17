2370dbd6eaa3f33ec904c0ef9a1e726eb46e70f7
Daniel Lesko <dles@ciklum.com>
2023-02-17 17:12:12 +0100

    initial cleanup and fetching data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const endpoint = "https://statsapi.web.nhl.com/api/v1/teams";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [nhlTeams, setNhlTeams] = useState();
+
+  async function getData() {
+    try {
+      const result = await axios.get(endpoint);
+      setNhlTeams(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(nhlTeams);
+
+  return <></>;
 }
 
 export default App;
